e7e9516eb0333dd11a28bd83fbbce589902220f4
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-06-13 22:05:44 -0700

    publish fixes

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,7 +38,7 @@ function Bio() {
               }}
             />
             <p>
-              Wriy <strong>{author}</strong> who lives and works in Los Angeles!! building useful things.
+              Wy <strong>{author}</strong> who lives and works in Los Angeles!! building useful things.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
